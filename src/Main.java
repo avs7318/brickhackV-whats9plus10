3d4e4af01cb9368335ee8199ce7dcebaedf0fcbb
Alex Scholeno <avs7318@g.rit.edu>
2019-02-16 12:09:55 -0500

    Add gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("BrickHack");
         System.out.println("Hello World");
         System.out.println("What is 9 + 10 fellas?");    }
 }
